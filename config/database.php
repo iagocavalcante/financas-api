d71363d011f8913f562f97f47c484e7b199125fb
Iago Cavalcante <iagoangelimc@gmail.com>
2017-06-29 22:31:30 -0300

    Mudanças no database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'ec2-54-225-118-55.compute-1.amazonaws.com'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'dc8uju6l6evv0n'),
-            'username' => env('DB_USERNAME', 'cnzyrvezrqonzh'),
-            'password' => env('DB_PASSWORD', 'b82c85815976824e17974ad74385edc268159f1b7c46fafb36e5f07e559f8d10'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-54-225-118-55.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'dc8uju6l6evv0n'),
+            'username' => env('DB_USERNAME', 'cnzyrvezrqonzh'),
+            'password' => env('DB_PASSWORD', 'b82c85815976824e17974ad74385edc268159f1b7c46fafb36e5f07e559f8d10'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
