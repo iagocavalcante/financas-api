c76da116dd56ae93b7dab9f441ad5d430c299557
Iago Cavalcante <iagoangelimc@gmail.com>
2017-06-29 22:27:54 -0300

    Mudanças no database

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', 'ec2-54-225-118-55.compute-1.amazonaws.com'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'dc8uju6l6evv0n'),
+            'username' => env('DB_USERNAME', 'cnzyrvezrqonzh'),
+            'password' => env('DB_PASSWORD', 'b82c85815976824e17974ad74385edc268159f1b7c46fafb36e5f07e559f8d10'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
